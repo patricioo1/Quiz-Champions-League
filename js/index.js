bfda718cad8cc9d51595a438516eb8554c11eca7
patricioo1 <patryksmolarz1@gmail.com>
2021-05-05 20:19:04 +0200

    new json file

--- js/index.js
+++ js/index.js
@@ -1,118 +1,9 @@
-import { scoreState, questionState } from './constants.js';
+import {
+    scoreState,
+    questionState
+} from './states.js';
 const scoreModule = scoreState(1);
 const questionModule = questionState();
-
-const Questions = [{
-        question: "Który zawodnik strzelił najwięcej goli w historii Ligi Mistrzów?",
-        answers: [
-            "Lionel Messi",
-            "Robert Lewandowski",
-            "Cristiano Ronaldo",
-            "Karim Benzema"
-        ],
-        correctAnswer: "Cristiano Ronaldo"
-    },
-    {
-        question: "Która drużyna jako pierwsza wygrała Puchar Ligi Mistrzów?",
-        answers: [
-            "Bayern Monachium",
-            "FC Barcelona",
-            "Real Madryt",
-            "Legia Warszawa"
-        ],
-        correctAnswer: "Real Madryt"
-    },
-    {
-        question: "Jakim wynikiem zakończył się mecz w fazie grupowej Ligi Mistrzów pomiędzy Legią Warszawa a Borussią Dortmund, rozegrany 22 listopada 2016 roku?",
-        answers: [
-            "8:4",
-            "3:2",
-            "5:5",
-            "10:6",
-        ],
-        correctAnswer: "8:4"
-    },
-    {
-        question: "W którym roku został rozegrany finał Ligi Mistrzów FC Liverpool vs AC Milan?",
-        answers: [
-            "2004",
-            "2003",
-            "2005",
-            "2006"
-        ],
-        correctAnswer: "2005"
-    },
-    {
-        question: "Z jakim trenerem Chelsea Londyn zdobyła Ligę Mistrzów w 2012 roku?",
-        answers: [
-            "Andre Villas-Boas",
-            "Carlo Ancelotti",
-            "Rafael Benitez",
-            "Roberto Di Matteo"
-        ],
-        correctAnswer: "Roberto Di Matteo"
-    },
-    {
-        question: "Ilu polskich bramkarzy wygrało Ligę Mistrzów?",
-        answers: [
-            "2",
-            "3",
-            "4",
-            "5"
-        ],
-        correctAnswer: "4"
-    },
-    {
-        question: "Jakim wynikiem zakończył się finał Ligi Mistrzów w 2013 roku pomiędzy Bayernem Monachium a Borussią Dortmund, w którym brało udział trzech Polaków?",
-        answers: [
-            "2:0",
-            "2:1",
-            "3:2",
-            "1:0"
-        ],
-        correctAnswer: "2:1"
-    },
-    {
-        question: "Który pierwszy polski bramkarz wywalczył Puchar Ligi Mistrzów?",
-        answers: [
-            "Tomasz Kuszczak",
-            "Józef Młynarczyk",
-            "Artur Boruc",
-            "Jerzy Dudek"
-        ],
-        correctAnswer: "Józef Młynarczyk"
-    },
-    {
-        question: "Która polska drużyna wygrała w III rundzie kwalifikacyjnej do Ligi Mistrzów z FC Barceloną w 2008 roku?",
-        answers: [
-            "Wisła Kraków",
-            "Legia Warszawa",
-            "Lech Poznań",
-            "Piast Gliwice"
-        ],
-        correctAnswer: "Wisła Kraków"
-    },
-    {
-        question: "Który klub przegrał najwięcej razy finałowy mecz Ligi Mistrzów?",
-        answers: [
-            "Manchester United",
-            "Juventus Turyn",
-            "Borussia Dortmund",
-            "Atletico Madryt"
-        ],
-        correctAnswer: "Juventus Turyn"
-    },
-    {
-        question: "Który polski bramkarz wygrał Ligę Mistrzów z Manchesterem United?",
-        answers: [
-            "Tomasz Kuszczak",
-            "Łukasz Załuska",
-            "Wojciech Szczęsny",
-            "Łukasz Fabiański"
-        ],
-        correctAnswer: "Tomasz Kuszczak"
-    }
-];
 
 const startButton = document.querySelector(".Start__Question");
 const containerControls = document.querySelector('.Quiz__Controls');
@@ -131,28 +22,29 @@ let elementWasClicked = false;
 startButton.addEventListener('click', startGame);
 
 function startGame() {
-containerControls.classList.remove('hide');
-nextQuestion.classList.remove('hide');
-startButton.classList.add('hide');
-quizCounter.classList.remove('hide');
-getAnswerToButton(questionModule.getQuestion());
+    containerControls.classList.remove('hide');
+    nextQuestion.classList.remove('hide');
+    startButton.classList.add('hide');
+    quizCounter.classList.remove('hide');
+    getAnswerToButton(questionModule.getQuestionIndex());
 }
 
-const getAnswerToButton = (id) => {
-    console.log(id);
-    quizQuestion.innerHTML = Questions[id].question;
-    for (let answer = 0; answer < quizAnswers.length; answer++) {
-        quizAnswers[answer].innerHTML = Questions[id].answers[answer];
-    }
+const getAnswerToButton = (index) => {
+    const currentQuestion = questionModule.getQuestion(index);
+    quizQuestion.innerHTML = currentQuestion.question;
+    quizAnswers.forEach((answer, answerIndex) => {
+        answer.innerHTML = currentQuestion.answers[answerIndex];
+        console.log(answer, answerIndex);
+    })
 }
 
-for (let i = 0; i < quizAnswers.length; i++) {
-    quizAnswers[i].addEventListener('click', () => {
-        selectCorrectAnswer(i);
+
+quizAnswers.forEach((answer, answerIndex) => {
+    answer.addEventListener('click', () => {
+        selectCorrectAnswer(answerIndex);
         elementWasClicked = true;
-        console.log(elementWasClicked);
-    });
-};
+    })
+})
 
 nextQuestion.addEventListener('click', () => {
     if (!elementWasClicked) {
@@ -162,13 +54,14 @@ nextQuestion.addEventListener('click', () => {
         questionModule.incrementQuestion();
         elementWasClicked = false;
     }
-    getAnswerToButton(questionModule.getQuestion());
+    const currentQuestionIndex = questionModule.getQuestionIndex();
+    getAnswerToButton(currentQuestionIndex);
     isQuestionAnswered = true;
     quizAnswers.forEach(answer => {
         answer.classList.remove('correct');
         answer.classList.remove('wrong');
     });
-    if (questionModule.getQuestion() === Questions.length -1) {
+    if (currentQuestionIndex === questionModule.getNumberOfQuestions() - 1) {
         console.log('to jest ostatnie pytanie');
         finishQuestion.classList.remove('hide');
         nextQuestion.classList.add('hide');
@@ -176,20 +69,21 @@ nextQuestion.addEventListener('click', () => {
 })
 
 
-const selectCorrectAnswer = (index) => {
-    const currentQuestion = Questions[questionModule.getQuestion()];
+const selectCorrectAnswer = (selectedAnswerIndex) => {
+    const currentQuestion = questionModule.getQuestion(questionModule.getQuestionIndex());
     for (let i = 0; i < currentQuestion.answers.length; i++) {
-        console.log(index);
-   if (currentQuestion.answers[i] === currentQuestion.correctAnswer) {
-       console.log('poprawna');
-       quizAnswers[i].classList.add('correct');
-   } else {
-       console.log('niepoprawna');
-       quizAnswers[i].classList.add('wrong');
-   }
+        console.log(selectedAnswerIndex);
+        if (currentQuestion.answers[i] === currentQuestion.correctAnswer) {
+            console.log('poprawna');
+            console.log(currentQuestion.answers[i]);
+            quizAnswers[i].classList.add('correct');
+        } else {
+            console.log('niepoprawna');
+            quizAnswers[i].classList.add('wrong');
+        }
     }
-    const selectedAnswerText = currentQuestion.answers[index];
-    console.log(currentQuestion.answers[index]);
+    const selectedAnswerText = currentQuestion.answers[selectedAnswerIndex];
+    console.log(currentQuestion.answers[selectedAnswerIndex]);
     if (selectedAnswerText === currentQuestion.correctAnswer && isQuestionAnswered === true) {
         isQuestionAnswered = false;
         scoreModule.increment();
